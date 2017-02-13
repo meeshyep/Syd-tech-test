65469f5f4819b18aa119e0d282fa7c6cfa370ae4
Michelle Epstein <epstein.michelle@gmail.com>
2017-02-13 21:01:30 +0000

    Plateau test and logic

---
+++ lib/plateau.rb
@@ -0,0 +1,10 @@
+class Plateau
+    
+    attr_reader :max_x, :max_y
+    
+    def initialize(x,y)
+        @max_x = x
+        @max_y = y
+    end
+    
+end
--- spec/plateau_spec.rb
+++ spec/plateau_spec.rb
@@ -2,4 +2,12 @@ require 'plateau'
 
 describe Plateau do
     
+    context '#initialize new plateau' do
+        it 'starts from x and y coordinates' do 
+            plateau = described_class.new(5,6)
+            expect(plateau.plateau_max_x).to eq 5
+            expect(plateau.plateau_max_y).to eq 6
+        end
+    end
+    
 end
