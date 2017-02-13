3e0ac7f9c14d19e76d3bb81a205f1cb574fcf565
Michelle Epstein <epstein.michelle@gmail.com>
2017-02-13 23:00:18 +0000

    Added rover coordinates and direction

--- lib/rover.rb
+++ lib/rover.rb
@@ -2,14 +2,32 @@ class Rover
     
     COMPASS = ['N', 'E', 'S', 'W']
     
-    attr_accessor = :x_coordinate, :y_coordinate 
+    attr_accessor = :x_coordinate, :y_coordinate, :direction 
     
     def initialize(x, y, direction)
         @x_coordinate = x
         @y_coordinate = y
         @direction = direction
     end
-  
+    
+    def give_instructions_to_rover(letter)
+     case letter
+        when "M" then move
+        when "R", "L" then turn(letter)
+        else raise "Direction provided is invalid"
+     end
+    end
+    
+    def turn(letter)
+     if letter == "R"
+      turn_to = (COMPASS.index(@direction) + 1) % 4
+      @direction = COMPASS[turn_to]
+     elsif letter == "L"
+      turn_to = (COMPASS.index(@direction) - 1) % 4
+      @direction = COMPASS[turn_to]
+     end
+    end
+    
     def move
      case @direction
        when "N" then @y_coordinate += 1
@@ -21,5 +39,6 @@ class Rover
     
     def state_coordinates_and_direction
      "#{@x_coordinate}, #{@y_coordinate}, pointing #{@direction}"
-   end
+    end
+    
 end 
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,6 +1,7 @@
 require 'rover'
 
 describe Rover do 
+    
     context '#initialise new rover' do
      it 'starts with x,y coordinates and compass' do
        rover = described_class.new(0,0,"N")
@@ -10,20 +11,43 @@ describe Rover do
      end
    end
  
-   context '#move' do
+    context '#move' do
      it 'will move to navigation points' do
        rover = described_class.new(4,8,"N")
        rover.move
        expect(rover.x_coordinate).to eq 4
        expect(rover.y_coordinate).to eq 8
      end
-   end
+    end
    
-   context '#states the coordinates and compass' do
-     it 'can state it\'s coordinates and compass' do
-       rover = described_class.new(4,8,"N")
-       expect(rover.state_coordinates_and_direction).to eq "4, 8, pointing N"
+    context '#change_direction' do
+     it 'can turn right' do
+       rover = described_class.new(1,3,"N")
+       rover.turn_right
+       expect(rover.direction).to eq "E"
+     end
+
+     it 'can turn left' do
+       rover = described_class.new(1,3,"N")
+       rover.turn_left
+       expect(rover.direction).to eq "L"
      end
    end
+   
+    context '#states the coordinates and direction' do
+      it 'can state it\'s coordinates and direction' do
+        rover = described_class.new(4,8,"N")
+        expect(rover.state_coordinates_and_direction).to eq "4, 8, pointing N"
+      end
+    end
+
+    context '#give instructions' do
+     it 'understands a series of instructions stating how to explore the plateau' do
+      rover = described_class.new(1,3,"N")
+      instructions = ["L", "M", "L", "M", "L", "M", "L", "M", "M"]
+      rover.give_instructions_to_rover(instructions)
+      expect(rover.state_coordinates_and_direction).to eq "1, 4, pointing N"
+     end
+    end
     
 end
