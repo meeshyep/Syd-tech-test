8f513695da893ff4ce1c5e7a349791e0693d38d5
Michelle Epstein <epstein.michelle@gmail.com>
2017-02-14 16:16:43 +0000

    All tests passing

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -13,24 +13,24 @@ describe Rover do
  
     context '#move' do
      it 'will move to navigation points' do
-       rover = described_class.new(4,8,"N")
+       rover = described_class.new(0,0,"N")
        rover.move
-       expect(rover.x_coordinate).to eq 4
-       expect(rover.y_coordinate).to eq 8
+       expect(rover.x_coordinate).to eq 0
+       expect(rover.y_coordinate).to eq 1
      end
     end
    
     context '#change_direction' do
      it 'can turn right' do
        rover = described_class.new(1,3,"N")
-       rover.turn_right
+       rover.turn("R")
        expect(rover.direction).to eq "E"
      end
 
      it 'can turn left' do
        rover = described_class.new(1,3,"N")
-       rover.turn_left
-       expect(rover.direction).to eq "L"
+       rover.turn("L")
+       expect(rover.direction).to eq "W"
      end
    end
    
@@ -44,8 +44,10 @@ describe Rover do
     context '#give instructions' do
      it 'understands a series of instructions stating how to explore the plateau' do
       rover = described_class.new(1,3,"N")
-      instructions = ["L", "M", "L", "M", "L", "M", "L", "M", "M"]
-      rover.give_instructions_to_rover(instructions)
+      letter = ["L", "M", "L", "M", "L", "M", "L", "M", "M"]
+      letter.each do |letter|
+      rover.give_instructions_to_rover(letter)
+     end
       expect(rover.state_coordinates_and_direction).to eq "1, 4, pointing N"
      end
     end
